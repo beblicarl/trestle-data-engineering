1855870f0f640a28afc2b1412f032d83ac41e8dc
Carl Bebli <beblicarl.cb@gmail.com>
2024-04-02 11:37:14 +0000

    improved code

--- robert-bosch-data-generator/index.py
+++ robert-bosch-data-generator/index.py
@@ -2,33 +2,40 @@ from faker import Faker
 import pandas as pd
 import random
 
-# Initialize Faker and seed for reproducibility
 fake = Faker()
-Faker.seed(0)
 
-# Generate 100k records 
-num_records = 100000
+# Generate 120k records
+num_records = 120000
 
 data = []
 for _ in range(num_records):
     company_name = 'Robert Bosch Ghana Limited'
     name = fake.name()
+    phone = fake.phone_number()
+    email = fake.email()
     address = ['Greater Accra Region', 'Ashanti Region', 'Western Region', 'Eastern Region', 'Central Region', 'Volta Region',
                'Northern Region', 'Upper East Region', 'Upper West Region', 'Bono Region', 'Ahafo Region', 'Bono East Region',
                'Savannah Region', 'North East Region', 'Oti Region', 'Western North Region']
-    transaction_activity = ['auto parts & accessories', 'eBike systems', 'motor vehicle technology', 'household appliances',
+    branch = 'Airport Residential Area'
+    service_product = ['auto parts & accessories', 'eBike systems', 'motor vehicle technology', 'household appliances',
                             'security systems', 'solar inverters', 'packaging technology', 'industry solutions',
                             'business process management solutions']
+    transaction_activity = fake.random_int(min=1, max=9999)
     customer_preference = ['website', 'facebook', 'linkedin', 'youtube', 'x', 'instagram']
     communication_method = ['email', 'phone']
 
     data.append({
         'Company': company_name,
         'Name': name,
+        'Phone Number': phone,
+        'Email Address' : email,
         'Address': random.choice(address),
-        'Transaction Activity': random.choice(transaction_activity),
+        'Branch' : branch,
+        'Services / Products': random.choice(service_product),
+        'Transaction Activity': transaction_activity,
         'Customer Preference': random.choice(customer_preference),
-        'Communication Method': random.choice(communication_method)
+        'Communication Method': random.choice(communication_method),
+        
     })
 
 # Create a DataFrame from the generated data
